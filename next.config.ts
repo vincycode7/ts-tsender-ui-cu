2bff67ae75caac478c68e9866b28739ec1a46146
Gbenga Vincent Akinwande <gbengavincentakinwande@gmail.com>
2025-09-05 20:37:50 +0100

    final changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "./",
+  trailingSlash: true,
 };
 
 export default nextConfig;
